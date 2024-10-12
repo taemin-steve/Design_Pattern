6f35e69c0cee1bb8f968df8a6e9292136660a725
taemin-steve <okchrisok@naver.com>
2024-10-12 14:13:46 +0900

    feat : Add Comment

--- 08_PROXY/proxy1.cpp
+++ 08_PROXY/proxy1.cpp
@@ -20,6 +20,6 @@ int main()
 	Image* img = new Image("C:\\a.png");
 	img->draw();
 
-	int w = img->width();
-	int h = img->height();
+	int w = img->width(); // 이것만 확인하고 싶음! 
+	int h = img->height(); // 하지만 해당 동작을 수행하기 위해서는 메모리에 전체 객체를 로드해 주어야함 
 }
--- 08_PROXY/proxy2.cpp
+++ 08_PROXY/proxy2.cpp
@@ -35,10 +35,16 @@ public:
 	int width()  const { return 100;} // 파일헤더에서 정보 획득
 	int height() const { return 100;} // 파일헤더에서 정보 획득
 
+	// 인증을 남길수도 있고 
+	// 보안, 원격지 서버에 대한 대행자등 다양한 기능을 추가할 수 있음. 
+	// 브릿지와 유사하지만 브릿지는 업데이트를 위함. 
+	// 데코레이터는 중첩의 형태로 추가, 인테페이스 동일 >> 프록시는 중첩 x, 이터페이스는 같을 수도 있고 다를 수도 있음. 
+	
 	void draw() 
 	{
 		if ( img == nullptr )
-			img = new Image(name);
+			img = new Image(name); // 꼭 그림을 그려 주어야 할때는 img 객체를 생성해서 진행
+			// 지연된 생성이라 부름 
 
 		img->draw();
 	}
