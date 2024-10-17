5a22c075fce1295c9160cfb8a7e3794545abf64d
taemin-steve <okchrisok@naver.com>
2024-10-17 22:14:35 +0900

    feat : Add comment

--- 14_SINGLETON/singleton_gen2.cpp
+++ 14_SINGLETON/singleton_gen2.cpp
@@ -4,7 +4,7 @@ template<typename T> //CRTP라는 방법
 class Singleton
 {
 protected:
-	Singleton() {} // 싱속 받아서 사용할 수 있도록 변경해 주었음
+	Singleton() {} // 싱속 받아서 사용할 수 있도록 변경해 주었음 
 
 private:	
 	Singleton(const Singleton& ) = delete;
@@ -18,12 +18,12 @@ public:
 		std::lock_guard<std::mutex> g(m);
 
 		if ( instance == nullptr )
-			instance = new T;
+			instance = new T; // 전달받은 Type의 객체가 생성됨
 		
 		return *instance;
 	}
 };
-template<typename T> T* Singleton<T>::instance = nullptr; //추가적으로 해주어야함
+template<typename T> T* Singleton<T>::instance = nullptr; //T는 위의 클래스 내에서만 토용되므로 다음과같이 적어주어야 함 
 template<typename T> std::mutex Singleton<T>::m;
 
 
